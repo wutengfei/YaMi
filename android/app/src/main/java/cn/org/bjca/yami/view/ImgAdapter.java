05034188c6ee5b1dbba50f37ad3b3dccecea86c5
wutengfei <781862614@qq.com>
2018-05-09 17:15:46 +0800

    开始写滑动切换效果的主页

---
+++ android/app/src/main/java/cn/org/bjca/yami/view/ImgAdapter.java
@@ -0,0 +1,37 @@
+package cn.org.bjca.yami.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+import cn.org.bjca.yami.utils.ImageUtils;
+
+/**
+ * Created by Administrator on 2016/9/13.
+ */
+public class ImgAdapter extends LoopVPAdapter<String> {
+
+    public ImgAdapter(Context context, ArrayList<String> datas, ViewPager viewPager) {
+        super(context, datas, viewPager);
+    }
+
+    private ViewGroup.LayoutParams layoutParams;
+
+    @Override
+    protected View getItemView(String data) {
+        if (layoutParams == null) {
+            layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        }
+        ImageView imageView = new ImageView(mContext);
+        imageView.setLayoutParams(layoutParams);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        ImageUtils.loadImage(mContext, data, imageView);
+        return imageView;
+    }
+
+
+}
